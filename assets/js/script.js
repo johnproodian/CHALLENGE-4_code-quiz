036bc4103d1ca9c0af2720293a780f307d236068
Jotas <jproodian721@gmail.com>
2021-12-01 12:41:50 -0600

    started dynamically adding html for quiz

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,9 @@
 var timerEl = document.getElementById('timer');
-var buttonEl = document.getElementById('start-button');
+var startButtonEl = document.getElementById('start-button');
 
 var timer = function() {
-    var timeLeft = 10;
+    // start the timer at 75s and have it count down until it reads 0
+    var timeLeft = 75;
     var timeInterval = setInterval(function() {
         timerEl.textContent = "Time: " + timeLeft;
         timeLeft = timeLeft - 1;
@@ -11,7 +12,22 @@ var timer = function() {
             clearInterval(timeInterval);
         }
     }, 1000);
+
+    // present the first question and listen for a click of the right answer
+    var quizQuestionEl = document.getElementById('quiz-question');
+    var blankPEl = document.getElementById('blank-p-element');
+    var startBtnLiEl = document.getElementById('start-btn-li');
+    var startQuiz = function() {
+        // dynamically create HTML text that replaces that of the current elements
+        quizQuestionEl.textContent = "This is the 1st question?";
+        blankPEl.textContent = "";
+        startButtonLiEl = // use removeChild() here, but might be for parent, not this element
+
+    }
+
+    startQuiz();
     
 }
 
-buttonEl.addEventListener("click", timer);
+startButtonEl.addEventListener("click", timer);
+
