65d51e9f702bb986b85f762da4dbc1298752124b
Jotas <jproodian721@gmail.com>
2021-12-02 23:53:02 -0600

    got the classes to change each question depending on what the right answer was.

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,26 +4,26 @@ var timerEl = document.getElementById('timer');
 var startButtonEl = document.getElementById('start-button');
 // create an array of all the questions and answers for select from below
 var qBank = [
-    "bThis is the first question?", 
-    "bThis is the 1-first answer.", 
-    "bThis is the 1-second answer.",
-    "bThis is the 1-third answer.",
-    "-bThis is the 1-fourth answer.",
+    "This is the first question?", 
+    "This is the 1-first answer.", 
+    "This is the 1-second answer.",
+    "This is the 1-third answer.",
+    "-This is the 1-fourth answer.",
     "This is the second question?",
     "-This is the 2-first answer.",
     "This is the 2-second answer.",
     "This is the 2-third answer.",
     "This is the 2-fourth answer.",
-    "bThis is the third question?", 
-    "bThis is the 3-first answer.", 
-    "-bThis is the 3-second answer.",
-    "bThis is the 3-third answer.",
-    "bThis is the 3-fourth answer.",
-    "bThis is the fourth question?", 
-    "bThis is the 4-first answer.", 
-    "-bThis is the 4-second answer.",
-    "bThis is the 4-third answer.",
-    "bThis is the 4-fourth answer."
+    "This is the third question?", 
+    "This is the 3-first answer.", 
+    "-This is the 3-second answer.",
+    "This is the 3-third answer.",
+    "This is the 3-fourth answer.",
+    "This is the fourth question?", 
+    "This is the 4-first answer.", 
+    "-This is the 4-second answer.",
+    "This is the 4-third answer.",
+    "This is the 4-fourth answer."
     // 4, 1, 2, 2
 ]
 
@@ -38,6 +38,7 @@ var timer = function() {
         timerEl.textContent = "Time: " + timeLeft;
         if (timeLeft <= 0) {
             clearInterval(timeInterval);
+            timerEl.textContent = "Time: 0";
         }
         timeLeft = timeLeft - 1;
         
@@ -47,11 +48,12 @@ var timer = function() {
     var quizQuestionEl = document.getElementById('quiz-question');
     var PEl = document.getElementById('blank-p-element');
     var startBtnLiEl = document.getElementById('start-btn-li');
-    var mainEl = document.getElementById('main');
+    // Do I need this main element?
+    // var mainEl = document.getElementById('main');
     var answerOptionEl = document.getElementById('answer-options');
     
      
-    // function to present first question and listen for appropriate clicks for the next questions
+    // define function that removes original html and starts quiz (function)
     var startQuiz = function() {
         // function to dynamically remove the p element and original start button once the quiz starts
         var rmvPandBtnEls = function() {
@@ -61,105 +63,161 @@ var timer = function() {
         rmvPandBtnEls();
 
         var questionIndex = 0;
-        console.log(questionIndex);
-        // for (i = 0; i < qBank.length/5; i++) {
-            var quiz = function(a){
-                // dynamically add HTML elements and text that replaces that of the current elements
-                // edit h1 to contain the question - 0, 0+5, 0+10, etc., items in the qBank array
-                quizQuestionEl.textContent = qBank[a];
+        var classPicker = 0;
+
+        var quiz = function(a) {
+            // dynamically add HTML elements and text that replaces that of the current elements
+            // edit h1 to contain the question - 0, 0+5, 0+10, etc., items in the qBank array
+            quizQuestionEl.textContent = qBank[a];
+        
+            // dynamically create and fill the first answer button - 0+1,0+6,0+11, etc.
+            var answerLi1El = document.createElement("li");
+            answerLi1El.className = 'option';
+            var answerBtn1El = document.createElement("button");
+            answerBtn1El.textContent = qBank[a + 1];
+            answerLi1El.appendChild(answerBtn1El);
+            answerOptionEl.appendChild(answerLi1El);
+
+            // create and fill the second answer  - - 0+2,0+7,0+12, etc.
+            var answerLi2El = document.createElement("li");
+            answerLi2El.className = 'option'
+            var answerBtn2El = document.createElement("button")
+            answerBtn2El.textContent = qBank[a + 2];
+            answerLi2El.appendChild(answerBtn2El);
+            answerOptionEl.appendChild(answerLi2El);
+        
+            // create and fill the third answer button - 0+3,0+9,0+14, etc.
+            var answerLi3El = document.createElement("li");
+            answerLi3El.className = 'option';
+            var answerBtn3El = document.createElement("button");
+            answerBtn3El.textContent = qBank[a + 3];
+            answerLi3El.appendChild(answerBtn3El);
+            answerOptionEl.appendChild(answerLi3El);
+
+            // create and fill the fourth answer button - 0+4,0+10,0+15, etc.
+            var answerLi4El = document.createElement('li')
+            answerLi4El.className = 'option';
+            var answerBtn4El = document.createElement('button');
+            answerBtn4El.textContent = qBank[a + 4];
+            answerLi4El.appendChild(answerBtn4El);
+            answerOptionEl.appendChild(answerLi4El);
+
+            // assign 'correct' and 'wrong' classes to buttons of correct and wrong answers
+            var classAssign = [4, 1, 2, 2];
+            assignClassPicker = classAssign[classPicker];
             
-                // dynamically create and fill the first answer button - 0+1,0+6,0+11, etc.
-                var answerLi1El = document.createElement("li");
-                answerLi1El.className = 'option';
-                var answerBtn1El = document.createElement("button");
-                answerBtn1El.textContent = qBank[a + 1];
-                answerLi1El.appendChild(answerBtn1El);
-                answerOptionEl.appendChild(answerLi1El);
 
-                // create and fill the second answer  - - 0+2,0+7,0+12, etc.
-                var answerLi2El = document.createElement("li");
-                answerLi2El.className = 'option'
-                var answerBtn2El = document.createElement("button")
-                answerBtn2El.textContent = qBank[a + 2];
-                answerLi2El.appendChild(answerBtn2El);
-                answerOptionEl.appendChild(answerLi2El);
-            
-                // create and fill the third answer button - 0+3,0+9,0+14, etc.
-                var answerLi3El = document.createElement("li");
-                answerLi3El.className = 'option';
-                var answerBtn3El = document.createElement("button");
-                answerBtn3El.textContent = qBank[a + 3];
-                answerLi3El.appendChild(answerBtn3El);
-                answerOptionEl.appendChild(answerLi3El);
-
-                // create and fill the fourth answer button - 0+4,0+10,0+15, etc.
-                var answerLi4El = document.createElement('li')
-                answerLi4El.className = 'option';
-                var answerBtn4El = document.createElement('button');
-                answerBtn4El.textContent = qBank[a + 4];
-                answerLi4El.appendChild(answerBtn4El);
-                answerOptionEl.appendChild(answerLi4El);
-
-                // assign 'correct' and 'wrong' classes to buttons of correct and wrong answers
-                answerBtn4El.className = 'correct';
-                answerBtn1El.className = 'wrong';
-                answerBtn2El.className = 'wrong';
-                answerBtn3El.className = 'wrong';
-
-                // listen for the different 'clicks', adjust points or time based on right or wrong answers
-                var correctBtn = document.querySelector('.correct');
-                console.log(correctBtn);
-                var incorrectBtns = document.querySelectorAll('.wrong');
-                var incorrectBtnsArr = Array.from(incorrectBtns);
-                console.log(incorrectBtnsArr);
-                questionIndex = questionIndex + 5;
-                console.log(questionIndex);
-                var rmvOldQ = function() {
-                    answerLi1El.parentNode.removeChild(answerLi1El);
-                    answerLi2El.parentNode.removeChild(answerLi2El);
-                    answerLi3El.parentNode.removeChild(answerLi3El);
-                    answerLi4El.parentNode.removeChild(answerLi4El)
+            var assignClass = function(c) {
+                if (c === 1) {
+                    answerBtn1El.className += 'correct',
+                    answerBtn2El.className += 'wrong',
+                    answerBtn3El.className += 'wrong',
+                    answerBtn4El.className += 'wrong'
                 }
-                correctBtn.addEventListener('click', function() {
-                    score = score + 5;
-                    rmvOldQ();
-                    quiz(questionIndex);
-                    console.log(questionIndex);
-                    console.log(score);
-                })
-                incorrectBtnsArr[0].addEventListener('click', function() {
-                    timeLeft = timeLeft - 15;
-                    rmvOldQ();
-                    quiz(questionIndex);
-                    console.log(score);
-                })
-                incorrectBtnsArr[1].addEventListener('click', function() {
-                    timeLeft = timeLeft - 15;
-                    rmvOldQ();
-                    quiz(questionIndex);
-                    console.log(score);
-                })
-                incorrectBtnsArr[2].addEventListener('click', function() {
-                    timeLeft = timeLeft - 15;
-                    rmvOldQ();
-                    quiz(questionIndex);
-                    console.log(score);
-                })
+                else if (c === 2) {
+                    answerBtn1El.className += 'wrong',
+                    answerBtn2El.className += 'correct',
+                    answerBtn3El.className += 'wrong',
+                    answerBtn4El.className += 'wrong'
+                }
+                else if (c === 3) {
+                    answerBtn1El.className += 'wrong',
+                    answerBtn2El.className += 'wrong',
+                    answerBtn3El.className += 'correct',
+                    answerBtn4El.className += 'wrong'
+                }
+                else if (c === 4) {
+                    answerBtn1El.className += 'wrong',
+                    answerBtn2El.className += 'wrong',
+                    answerBtn3El.className += 'wrong',
+                    answerBtn4El.className += 'correct'
+                }
+                console.log("1 = " + answerBtn1El, "2 = " + answerBtn2El, "3 = " + answerBtn3El, "4 = " + answerBtn4El);
             }
-            quiz(questionIndex)
-        // }
+
+            assignClass(assignClassPicker);
+            console.log("class names: " + answerBtn1El);
+
+            
+            
+            // listen for the different 'clicks', adjust points or time based on right or wrong answers
+            var correctBtn = document.querySelector('.correct');
+            var incorrectBtns = document.querySelectorAll('.wrong');
+            var incorrectBtnsArr = Array.from(incorrectBtns);
+            questionIndex = questionIndex + 5;
+
+            var rmvOldQ = function() {
+                answerLi1El.parentNode.removeChild(answerLi1El);
+                answerLi2El.parentNode.removeChild(answerLi2El);
+                answerLi3El.parentNode.removeChild(answerLi3El);
+                answerLi4El.parentNode.removeChild(answerLi4El)
+            }
+
+            correctBtn.addEventListener('click', function() {
+                score = score + 5;
+                rmvOldQ();
+                classPicker = classPicker + 1;
+                quiz(questionIndex);
+                console.log("Score: " + score);
+            })
+            incorrectBtnsArr[0].addEventListener('click', function() {
+                timeLeft = timeLeft - 15;
+                rmvOldQ();
+                classPicker = classPicker + 1;
+                quiz(questionIndex);
+                console.log("Score: " + score);
+            })
+            incorrectBtnsArr[1].addEventListener('click', function() {
+                timeLeft = timeLeft - 15;
+                rmvOldQ();
+                classPicker = classPicker +1;
+                quiz(questionIndex);
+                console.log("Score: " + score);
+            })
+            incorrectBtnsArr[2].addEventListener('click', function() {
+                timeLeft = timeLeft - 15;
+                rmvOldQ();
+                classPicker = classPicker +1;
+                quiz(questionIndex);
+                console.log("Score: " + score);
+            })
+        }
+        quiz(questionIndex);
         
       
     }
+
     startQuiz();
 }
     
-
-    
-
 startButtonEl.addEventListener("click", timer);
 
+//==========
 
+// So far, so good. 
+// Next:
+    // What I want to do next is to have the classes of the the buttons change ('correct'/'wrong') at each question.
+    // Hmmm. 
+    // At each click, add 1 to the question-select variable.
+    // At the start of the quiz function, use if or while (?) to assign classes.
+        // if (question-select === 1, 3) {Btn1, Btn2, Btn4 .className = 'wrong'; Btn3.className = 'correct'}
+        // might have to remove classes as well...
+// Then:
+    // Add questions and answers to the qBank
+// Then:
+    // Figure out how to add to local storage and do inputs for high score
+// Then:
+    // Figure out end of game stuff--what happens if time runs out, what happens if all questions answered, etc.
+
+// I can't believe I might figure this out soon!!!!!!!!
+
+
+
+
+
+
+
+//===========
 
 // ---
 
