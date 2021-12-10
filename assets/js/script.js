7e824a736f798e1d0b2350537bbaaa514a27f6ac
Jotas <jproodian721@gmail.com>
2021-12-10 00:16:25 -0600

    got high score page to work again, was able to pinpoint issue with local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,19 @@
+// Issues:
+    // (1) If there is no local storage already, then there seems to be issues
+    // (2) if there is local storage, the previous initials and scores don't seem to get added to the initials and scores array in the for loop
+    // (3) B/C of (2), the local storage is just replaced each time, never added to.
+
+
+
+
+
+
+// // var testScores = {
+//     init: "jjp",
+//     sco: 10
+// };
+// testScores = JSON.stringify(testScores);
+// localStorage.setItem("initialsAndScores", testScores);
 // get the time li to be able to add the text of current time to it in the timer function
 var timerEl = document.getElementById('timer');
 // get the start button to be able to listen to it being clicked below
@@ -39,6 +55,7 @@ var qBank = [
 
 // starting score for the user
 var  score = 0;
+var initialsAndScores = [];
 
 // get the various elements that will be dynamically manipulated to go through the quiz
 var quizQuestionEl = document.getElementById('quiz-question');
@@ -47,7 +64,7 @@ var startBtnLiEl = document.getElementById('start-btn-li');
 var mainEl = document.getElementById('main');
 var answerOptionEl = document.getElementById('answer-options');
 
-var initialsAndScores = [];
+
 
 var stopTimer = function(a) {
     clearInterval(a);
@@ -79,21 +96,23 @@ var stopTimer = function(a) {
             submitScoreBtn.addEventListener("click", function(event) {
                 event.preventDefault();
                 var initials = document.querySelector("input[name='initials']").value;
-
+                console.log(initials);
+                debugger;
                 var initScoObj = {
                     inits: initials,
                     sco: score
                 }
-
+                // debugger;
                 var previousInitialsAndScores = localStorage.getItem("initialsAndScores");
                 if (previousInitialsAndScores) {
-                    previousInitialsAndScores = JSON.parse(previousInitialsAndScores);
+                    previousInitialsAndScores = JSON.parse(previousInitialsAndScores); // here is where the (or, a) problem lies --> issue was that
+                                                                                        // JSON has to parse something in object format...
                     for (i = 0; i < previousInitialsAndScores; i++) {
-                        initialsAndScores.push(previousInitialsAndScores[i]);
+                        initialsAndScores.push(previousInitialsAndScores[i]);   // Seems here that the object that is previous initials and scores doesn't get pushed here...
                     }
                 }
 
-                initialsAndScores.push(initScoObj);
+                initialsAndScores.push(initScoObj);                             // But initScoObj DOES get pushed to initialsAndScores here...
                 console.log("initialsAndScores: " + JSON.stringify(initialsAndScores));
 
 
@@ -104,17 +123,17 @@ var stopTimer = function(a) {
                 //     initialsAndScores.push(savedInitialsAndScores);
                 // }
                 initialsAndScores = JSON.stringify(initialsAndScores);
-                localStorage.setItem("initialsAndScores", initialsAndScores);
+                localStorage.setItem("initialsAndScores", initialsAndScores);       // This REPLACES the previous locally-stored initialsAndScores b/c the previous push didn't work...
 
                 window.location.href = 'high-score.html';
     
             });
 };
 
-// define the timer function which will start the timer when the start button is clicked
-var startTimer = function() {
+
+// define function that removes original html and starts quiz (function), including the timer
+var startQuiz = function() {
     // start the timer at 75s and have it count down until it reads 0
-    
     var timeLeft = 75;
     var timeInterval = setInterval(function() {
         console.log(timeLeft);
@@ -125,150 +144,134 @@ var startTimer = function() {
         timeLeft = timeLeft - 1;
         
     }, 1000);
- 
+
+    // function to dynamically remove the p element and original start button once the quiz starts
+    var rmvPandBtnEls = function() {
+        pEl.textContent = "";
+        startBtnLiEl.parentNode.removeChild(startBtnLiEl);
+    }   
+    rmvPandBtnEls();
+
+    var questionIndex = 0;
+    var correctAssignIndex = 0;
+
+    var quiz = function(a) {
+        // dynamically add HTML elements and text that replaces that of the current elements
+        // edit h1 to contain the question - 0, 0+5, 0+10, etc., items in the qBank array
+        quizQuestionEl.textContent = qBank[a];
+            
+        // // dynamically create and fill the first answer button - 0+1,0+6,0+11, etc.
+        var answerLi1El = document.createElement("li");
+        var answerBtn1El = document.createElement("button");
+        answerBtn1El.textContent = qBank[a + 1];
+        answerLi1El.appendChild(answerBtn1El);
+        answerOptionEl.appendChild(answerLi1El);
+
+        // create and fill the second answer  - - 0+2,0+7,0+12, etc.
+        var answerLi2El = document.createElement("li");
+        var answerBtn2El = document.createElement("button")
+        answerBtn2El.textContent = qBank[a + 2];
+        answerLi2El.appendChild(answerBtn2El);
+        answerOptionEl.appendChild(answerLi2El);
     
-    
-     
-    // define function that removes original html and starts quiz (function)
-    var startQuiz = function() {
-        // function to dynamically remove the p element and original start button once the quiz starts
-        var rmvPandBtnEls = function() {
-            pEl.textContent = "";
-            startBtnLiEl.parentNode.removeChild(startBtnLiEl);
-        }   
-        rmvPandBtnEls();
+        // create and fill the third answer button - 0+3,0+9,0+14, etc.
+        var answerLi3El = document.createElement("li");
+        var answerBtn3El = document.createElement("button");
+        answerBtn3El.textContent = qBank[a + 3];
+        answerLi3El.appendChild(answerBtn3El);
+        answerOptionEl.appendChild(answerLi3El);
 
-        var questionIndex = 0;
-        var correctAssignIndex = 0;
+        // create and fill the fourth answer button - 0+4,0+10,0+15, etc.
+        var answerLi4El = document.createElement('li')
+        var answerBtn4El = document.createElement('button');
+        answerBtn4El.textContent = qBank[a + 4];
+        answerLi4El.appendChild(answerBtn4El);
+        answerOptionEl.appendChild(answerLi4El);
 
-        var quiz = function(a) {
-            // dynamically add HTML elements and text that replaces that of the current elements
-            // edit h1 to contain the question - 0, 0+5, 0+10, etc., items in the qBank array
-            quizQuestionEl.textContent = qBank[a];
-                
-            // // dynamically create and fill the first answer button - 0+1,0+6,0+11, etc.
-            var answerLi1El = document.createElement("li");
-            var answerBtn1El = document.createElement("button");
-            answerBtn1El.textContent = qBank[a + 1];
-            answerLi1El.appendChild(answerBtn1El);
-            answerOptionEl.appendChild(answerLi1El);
-
-            // create and fill the second answer  - - 0+2,0+7,0+12, etc.
-            var answerLi2El = document.createElement("li");
-            var answerBtn2El = document.createElement("button")
-            answerBtn2El.textContent = qBank[a + 2];
-            answerLi2El.appendChild(answerBtn2El);
-            answerOptionEl.appendChild(answerLi2El);
+        // assign 'correct' and 'wrong' classes to buttons of correct and wrong answers
+        var correctAssign = [4, 3, 4, 3, 1, 1];
+        var assignCorrectPicker = correctAssign[correctAssignIndex];
         
-            // create and fill the third answer button - 0+3,0+9,0+14, etc.
-            var answerLi3El = document.createElement("li");
-            var answerBtn3El = document.createElement("button");
-            answerBtn3El.textContent = qBank[a + 3];
-            answerLi3El.appendChild(answerBtn3El);
-            answerOptionEl.appendChild(answerLi3El);
 
-            // create and fill the fourth answer button - 0+4,0+10,0+15, etc.
-            var answerLi4El = document.createElement('li')
-            var answerBtn4El = document.createElement('button');
-            answerBtn4El.textContent = qBank[a + 4];
-            answerLi4El.appendChild(answerBtn4El);
-            answerOptionEl.appendChild(answerLi4El);
-
-            // assign 'correct' and 'wrong' classes to buttons of correct and wrong answers
-            var correctAssign = [4, 3, 4, 3, 1, 1];
-            var assignCorrectPicker = correctAssign[correctAssignIndex];
-            
-
-            var assignCorrect = function(c) {
-                if (c === 1) {
-                    answerBtn1El.className = 'correct option',
-                    answerBtn2El.className = 'wrong option',
-                    answerBtn3El.className = 'wrong option',
-                    answerBtn4El.className = 'wrong option'
-                }
-                else if (c === 2) {
-                    answerBtn1El.className = 'wrong option',
-                    answerBtn2El.className = 'correct option',
-                    answerBtn3El.className = 'wrong option',
-                    answerBtn4El.className = 'wrong option'
-                }
-                else if (c === 3) {
-                    answerBtn1El.className = 'wrong option',
-                    answerBtn2El.className = 'wrong option',
-                    answerBtn3El.className = 'correct option',
-                    answerBtn4El.className = 'wrong option'
-                }
-                else if (c === 4) {
-                    answerBtn1El.className = 'wrong option',
-                    answerBtn2El.className = 'wrong option',
-                    answerBtn3El.className = 'wrong option',
-                    answerBtn4El.className = 'correct option'
-                }
+        var assignCorrect = function(c) {
+            if (c === 1) {
+                answerBtn1El.className = 'correct option',
+                answerBtn2El.className = 'wrong option',
+                answerBtn3El.className = 'wrong option',
+                answerBtn4El.className = 'wrong option'
             }
-
-            assignCorrect(assignCorrectPicker); 
-            
-            
-            // listen for the different 'clicks', adjust points or time based on right or wrong answers
-            var correctBtn = document.querySelector('.correct');
-            var incorrectBtns = document.querySelectorAll('.wrong');
-            var incorrectBtnsArr = Array.from(incorrectBtns);
-            questionIndex = questionIndex + 5;
-
-            var rmvOldQ = function() {
-                answerLi1El.parentNode.removeChild(answerLi1El);
-                answerLi2El.parentNode.removeChild(answerLi2El);
-                answerLi3El.parentNode.removeChild(answerLi3El);
-                answerLi4El.parentNode.removeChild(answerLi4El)
+            else if (c === 2) {
+                answerBtn1El.className = 'wrong option',
+                answerBtn2El.className = 'correct option',
+                answerBtn3El.className = 'wrong option',
+                answerBtn4El.className = 'wrong option'
             }
-
-            correctBtn.addEventListener('click', function() {
-                score = score + 5;
-                rmvOldQ();
-                correctAssignIndex = correctAssignIndex + 1;
-                if (correctAssignIndex >= correctAssign.length) {
-                    stopTimer(timeInterval);
-                }
-                quiz(questionIndex);
-                console.log("Score: " + score);
-            })
-            incorrectBtnsArr[0].addEventListener('click', function() {
-                timeLeft = timeLeft - 15;
-                rmvOldQ();
-                correctAssignIndex = correctAssignIndex + 1;
-                if (correctAssignIndex >= correctAssign.length) {
-                    stopTimer(timeInterval);
-                }
-                quiz(questionIndex);
-                console.log("Score: " + score);
-            })
-            incorrectBtnsArr[1].addEventListener('click', function() {
-                timeLeft = timeLeft - 15;
-                rmvOldQ();
-                correctAssignIndex = correctAssignIndex +1;
-                if (correctAssignIndex >= correctAssign.length) {
-                    stopTimer(timeInterval);
-                }
-                quiz(questionIndex);
-                console.log("Score: " + score);
-            })
-            incorrectBtnsArr[2].addEventListener('click', function() {
-                timeLeft = timeLeft - 15;
-                rmvOldQ();
-                correctAssignIndex = correctAssignIndex +1;
-                if (correctAssignIndex >= correctAssign.length) {
-                    stopTimer(timeInterval);}
-                quiz(questionIndex);
-                console.log("Score: " + score);
-            })
+            else if (c === 3) {
+                answerBtn1El.className = 'wrong option',
+                answerBtn2El.className = 'wrong option',
+                answerBtn3El.className = 'correct option',
+                answerBtn4El.className = 'wrong option'
+            }
+            else if (c === 4) {
+                answerBtn1El.className = 'wrong option',
+                answerBtn2El.className = 'wrong option',
+                answerBtn3El.className = 'wrong option',
+                answerBtn4El.className = 'correct option'
+            }
         }
-        quiz(questionIndex);
-    }
 
-    startQuiz();
+        assignCorrect(assignCorrectPicker); 
+        
+        
+        // listen for the different 'clicks', adjust points or time based on right or wrong answers
+        var correctBtn = document.querySelector('.correct');
+        var incorrectBtns = document.querySelectorAll('.wrong');
+        var incorrectBtnsArr = Array.from(incorrectBtns);
+        questionIndex = questionIndex + 5;
+
+        var rmvOldQ = function() {
+            answerLi1El.parentNode.removeChild(answerLi1El);
+            answerLi2El.parentNode.removeChild(answerLi2El);
+            answerLi3El.parentNode.removeChild(answerLi3El);
+            answerLi4El.parentNode.removeChild(answerLi4El)
+        }
+
+        var incorrectClick = function() {
+            timeLeft = timeLeft - 15;
+            rmvOldQ();
+            correctAssignIndex = correctAssignIndex +1;
+            if (correctAssignIndex >= correctAssign.length) {
+                stopTimer(timeInterval);
+            }
+            quiz(questionIndex);
+            console.log("Score: " + score);
+        }
+
+        correctBtn.addEventListener('click', function() {
+            score = score + 5;
+            rmvOldQ();
+            correctAssignIndex = correctAssignIndex + 1;
+            if (correctAssignIndex >= correctAssign.length) {
+                stopTimer(timeInterval);
+            }
+            quiz(questionIndex);
+            console.log("Score: " + score);
+        })
+        incorrectBtnsArr[0].addEventListener('click', function() {
+            incorrectClick();
+        })
+        incorrectBtnsArr[1].addEventListener('click', function() {
+            incorrectClick();
+        })
+        incorrectBtnsArr[2].addEventListener('click', function() {
+            incorrectClick();
+        })
+    }
+    quiz(questionIndex);
 }
+
     
-startButtonEl.addEventListener("click", startTimer);
+startButtonEl.addEventListener("click", startQuiz);
 
 
 // What would the function look like to go to the HS form
@@ -280,39 +283,8 @@ var getScoreInitials = function() {
 }
 
 
-// ==========
-
-// So, what is supposed to happen w/ keeping track of scores, local storage, etc.?
-    // Games ends--run out of Q's, run out of time [if statement for both?]
-    // Take value of score variable at that point and display a message (confirm) that says the quiz is over and your final score is 'score'; do you want to save it?
-    // If no, display message to come back or ask to try again.
-    // If yes, take user to screen where they can create a high score item just like a taskinator to-do
-        // Dynamically change html/css to something more like taskinator:
-            // h1 (stay), p (add back? don't remeove it in the first place but make it blank?), form (new element), submit btn (redo it or don't get rid of it in the first place?)
-        // Get initials with form
-        // Store initials in a variable and make an object w/ the score variable?
-        // Then store them together (as a string) in localStorage
-    // Then, take them to the high score page; two options:
-        // (1) create a new html page for the high scores
-        // (2) dynamically remove and add the html and css to present high score; have an event listener for coming home and restarting...??
-            // I say stick with the first one...
-        // MOdel the new high scores page on the taskinator page
 
 
 
-
-
-
-//==========
-
-// So far, so good. 
-// Classes change. Wooooo!
-
-// Next:
-    // Figure out local storage and adding a form that collects high scores and initials to store there.
-    // Figure out end-game things: what happens when time runs out, when questions are done (how about a bonus for finishing early?)
-    // Add the JS questions--maybe 10?
-    // Add adjustments from lessons I've completed since starting the challenge--to make code a lot cleaner, to have better solutions 
-    
 
 
